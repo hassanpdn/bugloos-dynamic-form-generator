e20da5a16e1997b8ed49309d67c24676a453c40d
hassan <hassanpudineh67@gmail.com>
2023-02-19 16:08:31 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Button from '@/components/Button.vue'
+
+export default defineComponent({
+      components: {
+            Button
+      }
+})
+</script>
+
+<template>
+  <Button />
+</template>
+
+<style scoped>
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [vue()],
+  test: {
+      globals: true,
+            environment: 'jsdom'
+      },
+  server: {
+    port: 4000
+  },
+})
