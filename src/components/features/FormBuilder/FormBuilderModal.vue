b3e395261d4d51032f7576f14dfe11267c3b2fe9
hassan <hassanpudineh67@gmail.com>
2023-02-28 13:14:00 +0330

    feat: add core field genertor using async/dynamic import to prevent unused components being imported

--- src/components/features/FormBuilder/FormBuilderModal.vue
+++ src/components/features/FormBuilder/FormBuilderModal.vue
@@ -1,24 +1,27 @@
 <template>
       <modal>
-            <h1 class="text-lg font-bold mb-3">Add new field to form</h1>
-            <Select v-model="selectedComponent" :options="options" :value="selectedComponent" />
+            <p class="text-lg font-bold mb-3 text-center">Add new field to form</p>
+            <select-input v-model="selectedComponent" label="Field Type" :options="componentNames"/>
+            {{ selectedComponent }}
       </modal>
 </template>
 
 <script lang="ts">
       import { defineComponent } from 'vue';
       import Modal from "@/components/shared/BaseModal/Modal.vue";
-      import Select from '@/components/shared/BaseSelect/Select.vue';
+      import SelectInput from '@/components/shared/BaseSelect/Select.vue';
 
       export default defineComponent({
             name: 'form-builder-modal',
-            components: { Modal, Select },
+            components: { Modal, SelectInput },
             data(){
                   return {
-                        // 'Checkbox', 'Text', 'Number', 'Date', 'Date Range', 'Radio', 'Textarea', 'Select'
-                        options: [
+                        componentNames: [
                               { name: 'Checkbox', value: 'CheckBoxInput' },
                               { name: 'Date', value: 'DateInput' },
+                              { name: 'Number', value: 'NumberInput' },
+                              { name: 'Radio', value: 'RadioInput' },
+                              { name: 'Textarea', value: 'Textarea' },     
                         ],
                         selectedComponent: 'Select Form Element'
                   }
--- src/components/shared/BaseModal/Modal.vue
+++ src/components/shared/BaseModal/Modal.vue
@@ -3,7 +3,7 @@
             <div class="w-full absolute h-screen top-0 flex">
                   <div @click="$emit('close')" class="absolute bg-black opacity-70 inset-0 z-0" ></div>
                   <div class="w-full max-w-lg p-3 relative mx-auto my-auto rounded-xl shadow-lg bg-white">
-                        <div class="text-center p-4 flex-auto justify-center leading-6"><slot/></div>
+                        <div class="p-4 flex-auto leading-6"><slot/></div>
                   </div>
             </div>
       </transition>
