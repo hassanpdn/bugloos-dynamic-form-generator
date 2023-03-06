515e4f84583afea7b640ba7b56d580d24fed2579
hassan <hassanpudineh67@gmail.com>
2023-03-06 14:09:56 +0330

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import './tailwind.css'
 import mitt from 'mitt';
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
 
+
 const emitter = mitt();
 const app = createApp(App);
+const pinia = createPinia();
 
 app.config.globalProperties.emitter = emitter;
 app.component('VueDatePicker', VueDatePicker);
-app.mount('#app')
+app.use(pinia).mount('#app');
